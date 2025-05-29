91bae02ecf3019d7c0d8689b8b97e65a40a6b211
John-Zou-IC <john.zou23@imperial.ac.uk>
2025-05-29 11:45:15 +0100

    Setting up ABC for Stochastic Process

--- stochastic_resetting/standard.py
+++ stochastic_resetting/standard.py
@@ -1,8 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from abc import ABC, abstractmethod
 
 
-class SingleDiffusionProcess:
+def plot_simulation(process, t, dt, f1=3.5, f2=2.5):
+    if not isinstance(process, StochasticProcess):
+        raise TypeError("Can not plot something which isn't a stochastic process")
+    coords = process.simulate(t, dt)
+    fig, ax = plt.subplots(1, 1, figsize = (f1, f2))
+    ax.plot(coords[0], coords[1])
+    min_val = np.min(coords[1])
+    max_val = np.max(coords[1])
+    if min_val >= 0:
+        ax.set_ylim([-max_val, max_val])
+    elif max_val <= 0:
+        ax.set_ylim([min_val, -min_val])
+    else:
+        val = np.max([-min_val, max_val])
+        ax.set_ylim([-val, val])
+
+
+class StochasticProcess(ABC):
+
+    def __init__(self, x0):
+        self.x0 = x0
+
+    @abstractmethod
+    def simulate(self, t, dt):
+        pass
+
+
+class SingleDiffusionProcess(StochasticProcess):
 
     def __init__(self, x0, D):
         self.x0 = x0
@@ -20,17 +48,3 @@ class SingleDiffusionProcess:
             pos_list.append(pos)
         pos_list = np.array(pos_list)
         return (time, pos_list)
-
-    def plot_simulation(self, t, dt, f1=3.5, f2=2.5):
-        coords = self.simulate(t, dt)
-        fig, ax = plt.subplots(1, 1, figsize = (f1, f2))
-        ax.plot(coords[0], coords[1])
-        min_val = np.min(coords[1])
-        max_val = np.max(coords[1])
-        if min_val >= 0:
-            ax.set_ylim([-max_val, max_val])
-        elif max_val <= 0:
-            ax.set_ylim([min_val, -min_val])
-        else:
-            val = np.max([-min_val, max_val])
-            ax.set_ylim([-val, val])
