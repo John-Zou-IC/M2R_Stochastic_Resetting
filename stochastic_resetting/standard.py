3304f4b766714577def3d75249717e9f72baf39f
John-Zou-IC <john.zou23@imperial.ac.uk>
2025-05-30 14:12:56 +0100

    Adding docstring

--- stochastic_resetting/standard.py
+++ stochastic_resetting/standard.py
@@ -48,7 +48,8 @@ class SingleDiffusionProcess(StochasticProcess):
         and variance 2*D.
         ---
         pos (float): Position of the particle.
-        dt (float): Small change in time used for simulation.'''
+        dt (float): Small change in time used for simulation.
+        '''
         return pos + normal_obs(0, 2*self.D) * np.sqrt(dt)
 
     def simulate(self, t, dt):
@@ -105,7 +106,7 @@ class SingleDiffusionProcess(StochasticProcess):
                 return time
         return None
 
-    def mfps_constant(self, target, iter=100, dt=0.1, tmax=100):
+    def mfps(self, target, iter=100, dt=0.1, tmax=100):
         '''
         Returns the sample mean of the first passage time of target
         based on a number of simulations of the single particle
@@ -153,7 +154,8 @@ class SingleDiffusionProcessConstantR(StochasticResetting):
         and variance 2*D.
         ---
         pos (float): Position of the particle.
-        dt (float): Small change in time used for simulation.'''
+        dt (float): Small change in time used for simulation.
+        '''
         if np.random.random() < self.r*dt:
             return 'reset'
         else:
@@ -229,7 +231,7 @@ class SingleDiffusionProcessConstantR(StochasticResetting):
                 return time
         return None
 
-    def mfpt_constant(self, target, iter=100, dt=0.1, tmax=100):
+    def mfpt(self, target, iter=100, dt=0.1, tmax=100):
         '''
         Returns the sample mean of the first passage time of target
         based on a number of simulations of the single particle
@@ -258,6 +260,15 @@ class FirstPassageError(ValueError):
 
 
 class SingleDiffusionProcessResetting(StochasticResetting):
+    '''
+    Defines a single particle diffusion process with markovian
+    but non-poissonian resetting.
+    ---
+    x0 (float): Initial position of the particle.
+    xr (float): Reset position of the particle.
+    D (float): Diffusion constant of the process.
+    r (function): The density function of the resetting process.
+    '''
 
     def __init__(self, x0, xr, D, r):
         self.x0 = x0
@@ -269,12 +280,34 @@ class SingleDiffusionProcessResetting(StochasticResetting):
                             "This can be a constant function.")
 
     def update_step(self, pos, t_reset, dt):
+        '''
+        Updates the position of the particle based on stochastic rules:
+        x(t + dt) = xr  with probability r(t_reset)*dt
+        x(t + dt) = x(t) + sqrt(dt) * N  with probability 1-r(t_reset)*dt
+        for N an observation from a normal random variable with mean 0
+        and variance 2*D.
+        ---
+        pos (float): Position of the particle.
+        t_reset (float): Time since last reset.
+        dt (float): Small change in time used for simulation.
+        '''
         if np.random.random() < self.r(t_reset)*dt:
             return 'reset'
         else:
             return pos + normal_obs(0, 2*self.D) * np.sqrt(dt)
 
     def simulate(self, t, dt):
+        '''
+        Simulates the diffusion process with resetting over a fixed period
+        of time using the stochastic rules:
+        x(t + dt) = xr  with probability r(t_reset)*dt
+        x(t + dt) = x(t) + sqrt(dt) * N  with probability 1-r(t_reset)*dt
+        for N an observation from a normal random variable with mean 0
+        and variance 2*D.
+        ---
+        t (float): Time over which the process is simulated.
+        dt (float): Small change in time used for simulation.
+        '''
         pos = self.x0
         t_reset = 0
         pos_list = [pos]
@@ -293,6 +326,16 @@ class SingleDiffusionProcessResetting(StochasticResetting):
         return (time, pos_list, reset_time, reset_pos)
 
     def plot_simulation(self, t, dt, f1=3.5, f2=2.5):
+        '''
+        Plots a typical simulation of the single particle diffusion process
+        with non-poissonian but markovian resetting.
+        ---
+        t (float): Time over which the process is simulated.
+        dt (float): Small change in time used for simulation.
+
+        f1 (float): the length of the figure.
+        f2 (float): the height of the figure.
+        '''
         coords = self.simulate(t, dt)
         fig, ax = figure_generation_one(coords, f1, f2, reset=self.xr)
         ax.plot(coords[0], coords[1])
@@ -303,3 +346,51 @@ class SingleDiffusionProcessResetting(StochasticResetting):
                           ymin=min(coords[3][index], self.xr),
                           ymax=max(coords[3][index], self.xr),
                           color='r')
+
+    def first_passage_simulation_constant(self, target, dt=0.1, tmax=100):
+        '''
+        Returns the first passage time of hitting the target based on
+        a simulation of the single particle diffusion process with 
+        non-poissonian but markovian resetting.
+        ---
+        target (float): Position of target to be reached.
+        dt (float): Small change in time used for simulation.
+        tmax (float): Maximum time of simulation.
+        '''
+        time = 0
+        t_reset = 0
+        pos = self.x0
+        cond = self.x0
+        while time < tmax:
+            pos = self.update_step(pos, t_reset, dt)
+            time += dt
+            t_reset += dt
+            if pos == 'reset':
+                cond = self.xr
+                pos = self.xr
+                t_reset = 0
+            if np.sign(cond-target) != np.sign(pos-target):
+                return time
+        return None
+
+    def mfpt(self, target, iter=100, dt=0.1, tmax=100):
+        '''
+        Returns the sample mean of the first passage time of target
+        based on a number of simulations of the single particle
+        diffusion process with non-poissonian but markovian resetting.
+        ---
+        target (float): Position of target to be reached.
+        iter (int): Number of iterations for simulation.
+        dt (float): Small change in time used for simulation.
+        tmax (float): Maximum time of simulation.
+        '''
+        count = 0
+        total = 0
+        for _ in range(iter):
+            time = self.first_passage_simulation_constant(target, dt, tmax)
+            if time is not None:
+                count += 1
+                total += time
+        if not count:
+            raise FirstPassageError('Target was never reached')
+        return total/count
