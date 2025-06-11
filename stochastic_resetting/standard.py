5c989728e12bcaf500a8b615dc8c24e233a59d57
John-Zou-IC <john.zou23@imperial.ac.uk>
2025-06-11 15:00:27 +0100

    Adjusting figures

--- stochastic_resetting/standard.py
+++ stochastic_resetting/standard.py
@@ -79,8 +79,8 @@ class SingleDiffusionProcess(StochasticProcess):
         '''
         coords = self.simulate(t, dt)
         fig, ax = figure_generation_one(coords, f1, f2)
+        fig.set_dpi(600)
         ax.plot(coords[0], coords[1])
-        ax.set_title(f"Diffusion Process: D = {self.D}")
         ax.set_ylabel("position")
         ax.set_xlabel("time")
 
@@ -198,6 +198,7 @@ class SingleDiffusionProcessConstantR(StochasticProcess):
         '''
         coords = self.simulate(t, dt)
         fig, ax = figure_generation_one(coords, f1, f2, reset=self.xr)
+        fig.set_dpi(600)
         ax.plot(coords[0], coords[1])
         if coords[2]:
             # Adding reset lines
@@ -206,7 +207,6 @@ class SingleDiffusionProcessConstantR(StochasticProcess):
                           ymin=min(coords[3][index], self.xr),
                           ymax=max(coords[3][index], self.xr),
                           color='r')
-        ax.set_title(f"Diffusion Process with Resetting: D = {self.D}, r = {self.r}")
         ax.set_ylabel("position")
         ax.set_xlabel("time")
 
@@ -222,11 +222,11 @@ class SingleDiffusionProcessConstantR(StochasticProcess):
                 vals[np.invert(cond_vec)] = vals[np.invert(cond_vec)] + normal_obs(0, 2*self.D, np.count_nonzero(np.invert(cond_vec))) * np.sqrt(dt)
         return vals
 
-    def NESS_Plot(self, n, t, dt, f1=3.5, f2=2.5, bins=20):
+    def NESS_Plot(self, n, t, dt, f1=3.5, f2=2.5, bins=20, range=None):
         vals = self.NESS(n, t, dt)
         fig, ax = plt.subplots(1, 1, figsize=(f1, f2))
-        ax.hist(vals, bins=bins, density=True, label="Simulation Result")
-        ax.set_title(f"NESS Approximation: D = {self.D}, r = {self.r}")
+        fig.set_dpi(600)
+        ax.hist(vals, bins=bins, density=True, label="Simulation Result", range=range)
 
     def first_passage_simulation_constant(self, target, dt=0.1, tmax=100):
         '''
